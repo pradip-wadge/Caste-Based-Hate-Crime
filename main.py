8bc2a542091533c31b8c93f4d0804eb4e34a073b
pradip-wadge <111996538+pradip-wadge@users.noreply.github.com>
2024-09-02 23:28:16 +0530

    main.py

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
+# importing labraries
 import pandas as pd
-import preprocessor
 import streamlit as st
-import plotly.express as px
+import preprocessor
 
 # changing the layout of streamlit
 st.set_page_config(layout="wide")
-st.sidebar.image("logo.png")
+
+# logo display
+st.sidebar.image("logo.jpg")
+
 # Load Crime data
 data = pd.read_csv("cleaned_data.csv")
+
+st.sidebar.markdown('---')  # Optional: Adds a horizontal line
 
 # creating sidebar
 st.sidebar.header("Filters")
@@ -27,17 +32,13 @@ selected_district = st.sidebar.selectbox('Select District', options=['All'] + li
 # Crime filter
 crime_type = st.sidebar.selectbox('Select Crime Type', options=['All'] + list(data.columns[4:-3]))
 
+# Display an info icon with expandable information
+with st.expander("ℹ️  About Dashboard"):
+    st.write("This dashboard presents an analysis of caste-related crimes in India, focusing on data from 2001 to 2013. It provides a detailed view of how crimes against Scheduled Castes (SC) and Scheduled Tribes (ST) were distributed across various regions during this period. The dashboard sheds light on the systemic issues tied to the Indian caste system, offering insights into the persistence of caste-based violence despite legal protections such as the SC/ST Act of 1989. The goal is to help understand the historical context of caste-related violence and the ongoing challenges faced by these marginalized communities.")
+
 # Main dashboard
 st.title("State & District - wise Crime Analysis Dashbord")
 st.markdown('-------') 
-# Custom styled title
-st.markdown(
-    """
-    <h1 style="color: #2c3e50; font-size: 30px; text-align: center;">
-        Caste Crimes : Crimes Against SC (2001-2013)
-    </h1>
-    """, unsafe_allow_html=True
-)
 
 
 # Global filtering
@@ -48,18 +49,42 @@ if selected_district != 'All':
     filtered_df = filtered_df[filtered_df['DISTRICT'] == selected_district]
 if crime_type != 'All':
     filtered_df = filtered_df[filtered_df[crime_type] > 0]
-
+   
 # Display map 
-preprocessor.display(filtered_df)
+if (selected_state != 'All') and (crime_type != 'All') and ((selected_district == 'All') or (selected_district != 'All')) :
+    df = filtered_df[["STATE/UT","DISTRICT",crime_type]].reset_index()
+    df.columns = ["index","STATE/UT","DISTRICT","Total Crimes"]
+    preprocessor.display(df)
+else :
+    preprocessor.display(filtered_df)
+
+st.markdown('-------') 
 
 # display bar graph for each state 
 if (selected_state == 'All') and (selected_district == 'All'):
     preprocessor.state_crime(filtered_df)
-elif selected_state != 'All':
-    preprocessor.district_crime(filtered_df) # gives district wise graph for each state
+elif (selected_state != 'All') and (crime_type == 'All'):
+    preprocessor.district_crime(filtered_df)
+    preprocessor.district_crime_type(filtered_df) # gives district wise graph for each state
+elif (selected_state != 'All') and (crime_type != 'All'):
+    df = filtered_df[["DISTRICT",crime_type]]
+    df.columns = ['District', 'Total']
+    preprocessor.crime_type(df) # gives specified crime distribution for specified state
+    
+
 st.markdown('-------') 
+
 # Pie chart display for each of filter
-preprocessor.Crime_year(filtered_df)
+if crime_type == 'All':
+    preprocessor.Crime_year(filtered_df)
+
+# Crime trends over time
+if crime_type == 'All':
+    crime_trend = filtered_df.groupby('Year').sum().reset_index()
+    preprocessor.Year_crime(crime_trend)
+else:
+    crime_trend = filtered_df.groupby('Year')[crime_type].sum().reset_index(name="Total Crimes")
+    preprocessor.Year_crime(crime_trend)
 
 # description at the bottom of crimes
 st.sidebar.markdown('---')  # Optional: Adds a horizontal line
@@ -69,11 +94,14 @@ if crime_type:
             """
             <div style="color: black; font-size: 18;">
                 <strong>Here :</strong><br>
-                 
-                1. Violent Crimes: Murder, Assault on Women, Kidnapping and Abduction, Hurt 
-                2. Property Crimes: Dacoity, Robbery, Arson 
+                1. Violent Crimes: Murder, Assault on Women, Kidnapping and Abduction, Hurt <br>
+                2. Property Crimes: Dacoity, Robbery, Arson <br>
                 3. Legal Acts and Protections: Prevention of Atrocities (POA) Act, Protection of Civil Rights (PCR) Act, Other Crimes Against SCs
             </div>
             """, unsafe_allow_html=True
         )
+st.sidebar.markdown('---')  # Optional: Adds a horizontal line
 
+# Url to visit 
+url = r"https://ncsk.nic.in/sites/default/files/PoA%20Act%20as%20amended-Nov2017.pdf"
+st.sidebar.write(f"[Click For More Informtion]({url})")
